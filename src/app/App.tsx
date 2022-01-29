fd5ddd76927b7fc10e00f27e39604a8f9e41f0cc
emVogel <michavogel80@gmail.com>
2022-01-29 20:51:02 +0100

    init project setup

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
-export class App extends React.Component<any, any> {
-  constructor(props: any) {
+export class App extends React.Component<unknown, unknown> {
+  constructor(props: unknown) {
     super(props);
   }
   public render(): JSX.Element {
