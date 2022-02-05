f704dab89cc38d6ec54215303f3321a81b9f8009
emVogel <michavogel80@gmail.com>
2022-02-05 22:40:36 +0100

    configure webpack for mode type

---
+++ README.md
@@ -0,0 +1,23 @@
+# A simple setup for react with typescript and webpack
+
+This is a simple setup to use for react projects based on typescript, webpack, eslint and prettier
+
+## How to use
+
+### Starting
+
+Run `npm run init` to get all the dependencies
+
+### Running the dev-server
+
+Run `npm run start` to start the dev server. The page reloads automatically on changes.
+
+### eslint and prettier
+
+Run `npm run lint` for eslint
+
+Run `npm run prettier:check` to check if your code is conform with prettier rules or `npm run prettier:fix` to format your code as defined by prettier rules.
+
+### build a package
+
+Run `npm run build:dev` for development or production `npm run build`
--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,61 @@
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: {
-    app: path.join(__dirname, "src", "index.tsx"),
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
-  },
-  target: "web",
-  devtool: "source-map",
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: "ts-loader",
-        exclude: "/node_modules",
-      },
-      {
-        test: /\.s?(a|c)ss$/,
-        use: [
-          "style-loader",
-          "css-loader",
-          {
-            loader: "sass-loader",
-            options: { implementation: require("sass") },
-          },
-        ],
-      },
-      {
-        test: /\.(gif|svg|jpg|png)$/,
-        loader: 'file-loader',
-      },
+module.exports = (_env, args) => { 
+  const isProd = args.mode === "production";
+
+  !isProd && console.log("running in dev mode")
+  const mode = args.mode || "development";
+
+  return {
+    mode: mode, 
+    entry: {
+      app: path.join(__dirname, "src", "index.tsx"),
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].js",
+    },
+    target: "web",
+    devtool: !isProd && "source-map",
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: "ts-loader",
+          exclude: "/node_modules",
+        },
+        {
+          test: /\.s?(a|c)ss$/,
+          use: [
+            "style-loader",
+            "css-loader",
+            {
+              loader: "sass-loader",
+              options: { implementation: require("sass") },
+            },
+          ],
+        },
+        {
+          test: /\.(gif|svg|jpg|png)$/,
+          loader: 'file-loader',
+        },
+      ],
+    },
+    devServer: {
+      port: 3000,
+
+    watchFiles: ["src/**"],
+
+    },
+
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: path.join(__dirname, "src/index.html"),
+      }),
     ],
-  },
-  devServer: {
-    port: 3000,
-
-   watchFiles: ["src/**"],
-
-  },
-
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: path.join(__dirname, "src/index.html"),
-    }),
-  ],
-};
+  }
+}
